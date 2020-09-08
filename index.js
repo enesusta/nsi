f3d5588580e48f768b36296178eee5f03412263b
Enes Usta <enesusta@email.com>
2020-09-08 14:26:42 +0300

    Fix existing .git subdirectory problem

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+#! /usr/bin/env node
 const spawn = require('cross-spawn');
+const rimraf = require('rimraf');
 const argv = process.argv.slice(2);
 
 spawn.sync('git', ['clone', 'https://github.com/enesusta/spring-boot-base', argv[0]], {
     stdio: 'inherit'
 });
+
+rimraf.sync(`${argv[0]}/.git`);
+
+console.log('Process has completed.');
