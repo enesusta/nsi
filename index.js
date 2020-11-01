96ab16d4aadd6bfa63b24c7ada493416e0ab9122
enesusta <enesusta@email.com>
2020-11-01 13:16:55 +0300

    add maven-basic-base

--- index.js
+++ index.js
@@ -3,10 +3,23 @@ const spawn = require('cross-spawn');
 const rimraf = require('rimraf');
 const argv = process.argv.slice(2);
 
-spawn.sync('git', ['clone', 'https://github.com/enesusta/spring-boot-base', argv[0]], {
-    stdio: 'inherit'
-});
+const repoInitializer = (repoUrl, repoName) => {
+    spawn.sync('git', ['clone', repoUrl, repoName], {
+        stdio: 'inherit'
+    });
 
-rimraf.sync(`${argv[0]}/.git`);
+    rimraf.sync(`${repoName}/.git`);
+}
+
+switch (argv[0]) {
+    case 'spring':
+        const springUrl = 'https://github.com/enesusta/spring-boot-base';
+        repoInitializer(springUrl, argv[1]);
+        break;
+    case 'maven':
+        const mavenUrl = 'https://github.com/enesusta/maven-basic-base';
+        repoInitializer(mavenUrl, argv[1]);
+        break;
+}
 
 console.log('Process has completed.');
